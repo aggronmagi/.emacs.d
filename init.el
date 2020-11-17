abed35bce84bef556a34f92714461f617f786032
czy <czy463@163.com>
2020-11-17 18:50:22 +0800

    emacs配置

---
+++ init.el
@@ -0,0 +1,18 @@
+
+;; 使用init.org 作为配置文件
+(org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
+
+;; 自动生成变量
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(lsp-treemacs treemacs-persp treemacs-magit treemacs-icons-dired treemacs-projectile treemacs projectile highlight-parentheses mwim git-gutter+ magit flycheck lsp-ui go-eldoc go-guru go-dlv yasnippet vterm thrift thrift-mode json-mode yaml-mode toml-mode dockerfile-mode protobuf-mode autopair ido-completing-read+ doom-modeline use-package rainbow-mode rainbow-delimiters exec-path-from-shell doom-themes)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
