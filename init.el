2db2cc0b1120de9f9bbb324c382e27c401e31d66
czy <czy463@163.com>
2020-11-18 14:59:27 +0800

    添加plantuml

--- init.el
+++ init.el
@@ -9,7 +9,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(vtm winum ivy-posframe counsel ivy yasnippet-snippets pandoc org-web-tools org-protocol-capture-html org-protocol lsp-treemacs treemacs-persp treemacs-magit treemacs-icons-dired treemacs-projectile treemacs projectile highlight-parentheses mwim git-gutter+ magit flycheck lsp-ui go-eldoc go-guru go-dlv yasnippet vterm thrift thrift-mode json-mode yaml-mode toml-mode dockerfile-mode protobuf-mode autopair ido-completing-read+ doom-modeline use-package rainbow-mode rainbow-delimiters exec-path-from-shell doom-themes)))
+   '(plantuml-mode vtm winum ivy-posframe counsel ivy yasnippet-snippets pandoc org-web-tools org-protocol-capture-html org-protocol lsp-treemacs treemacs-persp treemacs-magit treemacs-icons-dired treemacs-projectile treemacs projectile highlight-parentheses mwim git-gutter+ magit flycheck lsp-ui go-eldoc go-guru go-dlv yasnippet vterm thrift thrift-mode json-mode yaml-mode toml-mode dockerfile-mode protobuf-mode autopair ido-completing-read+ doom-modeline use-package rainbow-mode rainbow-delimiters exec-path-from-shell doom-themes)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
