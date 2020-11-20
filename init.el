10d23344d762daa94ae2744a1a0c8b51254f5c61
czy <czy463@163.com>
2020-11-20 18:27:47 +0800

    多列编辑.修复org-capture(只保存到临时文件.)

--- init.el
+++ init.el
@@ -1,15 +1,12 @@
 
 ;; 使用init.org 作为配置文件
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
-
-;; 自动生成变量
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(bm browse-kill-ring popwin youdao-dictionary org-bullets plantuml-mode vtm winum ivy-posframe counsel ivy yasnippet-snippets pandoc org-web-tools org-protocol-capture-html org-protocol lsp-treemacs treemacs-persp treemacs-magit treemacs-icons-dired treemacs-projectile treemacs projectile highlight-parentheses mwim git-gutter+ magit flycheck lsp-ui go-eldoc go-guru go-dlv yasnippet vterm thrift thrift-mode json-mode yaml-mode toml-mode dockerfile-mode protobuf-mode autopair ido-completing-read+ doom-modeline use-package rainbow-mode rainbow-delimiters exec-path-from-shell doom-themes)))
+ '(tab-width 4))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
